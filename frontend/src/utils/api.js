c32ddd61b593dc9170fbc44d62927de15d064b43
seinaugler-texas <seinaugler@gmail.com>
2026-03-23 21:46:07 -0500

    Update api.js

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+const BACKEND_URL = process.env.REACT_APP_BACKEND_URL || "https://perfect-essence-production-0ca7.up.railway.app";
+
 const api = axios.create({
-  baseURL: "/api",
+  baseURL: `${BACKEND_URL}/api`,
   headers: { "Content-Type": "application/json" },
 });
 
